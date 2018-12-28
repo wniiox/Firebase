e0e32d9e032dc40478fc253f76be1e30e6555e45
Wallace <wni.iox@gmail.com>
2018-12-27 19:52:11 -0500

    Initial commit

---
+++ firebase-project-A1/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'firebase-project-A1';
+  items: Observable<any[]>;
+  constructor(db: AngularFirestore) {
+    this.items = db.collection('test').valueChanges();
+
+  }
+}
---
+++ firebase-project-A1/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase, 'firebase-project-A1'),
+    AngularFirestoreModule,
+    //AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    //AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    AngularFireStorageModule // imports firebase/storage only needed for storage features
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
